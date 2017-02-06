e603cb5c1f9860675a02ab089f8b38d33d5f62f3
PolluxAvenger <polluxavenger@gmail.com>
2017-02-06 08:58:59 +0800

    hashlib 库
    MD5哈希的试用

---
+++ hashlib/md5_hash.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+import hashlib
+import zlib
+# hashlib 提供 RSA，MD5，SHA1，SHA224，SHA256，SHA384，SHA512 等标准哈希算法
+# zlib 提供 adler32 和 crc32 哈希
+
+if __name__ == '__main__':
+    text = b'Hello World!'
+    text_md5 = hashlib.md5(text).hexdigest()
+    # 要使用 md5 哈希算法，你必须传递字节串而不是普通的字符串
+    print(text_md5)
