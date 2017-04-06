7b9de22207c40340122ca65bdc98559cffa2833d
PolluxAvenger <polluxavenger@gmail.com>
2017-04-06 22:46:46 +0800

    SMTP 库
    SMTP 库发送邮件

--- smtplib/test.py
+++ smtplib/test.py
@@ -1,5 +1,24 @@
 # coding=utf-8
 
+import string
 import smtplib
 
+HOST = "smtp.gmail.com"
+SUBJECT = "Test email from Python"
+TO = "testmail@qq.com"
+FROM = "mymail@gmail.com"
+text = "Python rules them all!!!"
+BODY = string.join((
+    "From: %s" % FROM,
+    "To: %s" % TO,
+    "Subject: %s" % SUBJECT,
+    "",
+    text
+), "\r\n")
+
 server = smtplib.SMTP()
+server.connect(HOST, "25")
+server.starttls()
+server.login("mymail@gmail.com", "mypassword")
+server.sendmail(FROM, [TO], BODY)
+server.quit()
