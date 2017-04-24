d1dcc50729a96d241dc66875684c1ba42776902a
PolluxAvenger <polluxavenger@gmail.com>
2017-04-24 14:23:22 +0800

    string 库
    字符相关函数

---
+++ string/test.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import string
+
+result = string.ascii_lowercase
+print(result)
+result = string.ascii_uppercase
+print(result)
+result = string.digits
+print(result)
+result = string.hexdigits
+print(result)
+result = string.octdigits
+print(result)
+result = string.punctuation
+print(result)
+result = string.printable
+print(result)
+result = string.whitespace
+print(result)
