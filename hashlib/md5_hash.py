2263eb85341d5d56502d1442fc491aed6d5875f1
PolluxAvenger <polluxavenger@gmail.com>
2017-05-09 21:59:47 +0800

    hashlib 库
    增加对列表对象进行 SHA-256 计算

--- hashlib/md5_hash.py
+++ hashlib/md5_hash.py
@@ -9,4 +9,9 @@ if __name__ == '__main__':
     text = b'Hello World!'
     text_md5 = hashlib.md5(text).hexdigest()
     # 要使用 md5 哈希算法，你必须传递字节串而不是普通的字符串
-    print(text_md5)
+	print(text_md5)
+	
+	test_list = []
+	list_sha256 = hashlib.sha256()
+	list_sha256.update(repr(test_list).encode('utf-8')
+    print(list_sha256.hexdigest())
