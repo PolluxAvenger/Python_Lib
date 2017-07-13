1bea1104697b95ddf902d34a026014fab0b557a9
PolluxAvenger <polluxavenger@gmail.com>
2017-07-13 22:38:33 +0800

    dpkt 库

--- dpkt/test.py
+++ dpkt/test.py
@@ -3,3 +3,11 @@
 import dpkt
 
 if __name__=="__main__":
+	 with open(path, 'rb') as f_pcap:
+        pcap = dpkt.pcap.Reader(f_pcap)
+
+        count = 0
+
+        for timestamp, buf in pcap:
+            count += 1
+            eth = dpkt.ethernet.Ethernet(buf)
