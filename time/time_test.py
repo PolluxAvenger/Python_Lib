f8eda2e6972c0c779361b3093e6ec68ef5611686
PolluxAvenger <polluxavenger@gmail.com>
2018-02-23 22:21:42 +0800

    time 库
    格式化样板 strftime

--- time/time_test.py
+++ time/time_test.py
@@ -3,6 +3,7 @@
 import time
 
 print('当前时间是:' + time.ctime())
+print("格式化的当前时间为：，" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 print('当前的 UTC 时间:' + str(time.gmtime()))
 print('当前的本地时间:' + str(time.localtime()))
 print('当月的第几天:' + str(time.localtime().tm_mday))
